500fd7a6223eca5794dafa6358e9d3819d6f156c
dgstephenson <dgstephenson.econ@gmail.com>
2022-09-28 00:53:13 -0400

    setup

---
+++ public/client.js
@@ -0,0 +1,174 @@
+import { io } from './socketIo/socket.io.esm.min.js'
+const socket = io()
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+context.imageSmoothingEnabled = false
+
+socket.on('updateClient', (msg) => {
+  record.msg = msg
+  for (const property in msg.state) {
+    state[property] = msg.state[property]
+  }
+  camera.position = msg.position
+  const reply = { controls }
+  socket.emit('updateServer', reply)
+})
+
+// function range (n) { return [...Array(n).keys()] }
+// function clamp (a, b, x) { return Math.max(a, Math.min(b, x)) }
+// const sum = array => array.reduce((a, b) => a + b, 0)
+
+// Disable Right Click Menu
+document.oncontextmenu = () => false
+
+const mouse = {
+  down: [false, false, false],
+  x: 0,
+  y: 0
+}
+const controls = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+  select: false,
+  zoom: 0
+}
+const record = {}
+const keys = new Map()
+keys.set('w', 'up')
+keys.set('s', 'down')
+keys.set('a', 'left')
+keys.set('d', 'right')
+keys.set('ArrowUp', 'up')
+keys.set('ArrowDown', 'down')
+keys.set('ArrowLeft', 'left')
+keys.set('ArrowRight', 'right')
+keys.set(' ', 'select')
+keys.set('Enter', 'select')
+
+const camera = {
+  position: { x: 50, y: 50 }
+}
+const state = {}
+
+function updateMouse (e) {
+  const canvasRect = canvas.getBoundingClientRect()
+  mouse.y = Math.floor((e.pageY - canvasRect.top) * 100 / canvasRect.width)
+  mouse.x = Math.floor((e.pageX - canvasRect.left) * 100 / canvasRect.width)
+}
+
+window.onmousemove = function (e) {
+  updateMouse(e)
+}
+
+window.onmousedown = function (e) {
+  console.log('state', state)
+  // console.log('controls', controls)
+  if (e.button === 0) mouse.down[0] = true
+  if (e.button === 1) mouse.down[1] = true
+  if (e.button === 2) mouse.down[2] = true
+  updateMouse(e)
+}
+
+window.onmouseup = function (e) {
+  if (e.button === 0) mouse.down[0] = false
+  if (e.button === 1) mouse.down[1] = false
+  if (e.button === 2) mouse.down[2] = false
+}
+
+window.onkeydown = function (e) {
+  keys.forEach((value, key) => {
+    if (e.key === key) controls[value] = true
+  })
+}
+
+window.onkeyup = function (e) {
+  keys.forEach((value, key) => {
+    if (e.key === key) controls[value] = false
+  })
+}
+
+window.onwheel = function (e) {
+  controls.zoom -= e.deltaY / 1000
+}
+
+function setupCanvas () {
+  const size = 0.99 * Math.min(window.innerWidth, window.innerHeight)
+  canvas.width = size
+  canvas.height = size
+  const xTranslate = 0.5 * canvas.width
+  const yTranslate = 0.5 * canvas.height
+  const scale = Math.exp(controls.zoom)
+  const minSize = Math.min(window.innerHeight, window.innerWidth)
+  const xScale = scale * minSize / 100
+  const yScale = scale * minSize / 100
+  context.setTransform(xScale, 0, 0, yScale, xTranslate, yTranslate)
+  context.imageSmoothingEnabled = true
+}
+
+const colors = {
+  0: 'hsl(180, 100%, 80%)',
+  1: 'hsl(220, 100%, 50%)',
+  2: 'hsl(140, 100%, 30%)',
+  wall: 'hsl(180, 10%, 10%)'
+}
+
+function drawState () {
+  context.clearRect(0, 0, 100, 100)
+  if (state.nodes) {
+    context.globalAlpha = 0.01
+    state.nodes.forEach(node => {
+      context.beginPath()
+      context.fillStyle = colors[node.team]
+      const x = node.position.x - camera.position.x
+      const y = node.position.y - camera.position.y
+      context.arc(x, y, node.radius, 0, 2 * Math.PI)
+      context.fill()
+    })
+  }
+  if (state.walls) {
+    context.globalAlpha = 1
+    context.fillStyle = colors.wall
+    state.walls.forEach(wall => {
+      const x = wall.position.x - 0.5 * wall.width - camera.position.x
+      const y = wall.position.y - 0.5 * wall.height - camera.position.y
+      context.fillRect(x, y, wall.width, wall.height)
+    })
+  }
+  if (state.players) {
+    context.globalAlpha = 1
+    state.players.forEach(player => {
+      context.fillStyle = colors[player.team]
+      const x = player.position.x - camera.position.x
+      const y = player.position.y - camera.position.y
+      context.beginPath()
+      context.arc(x, y, player.radius, 0, 2 * Math.PI)
+      context.fill()
+      context.fillStyle = 'black'
+      const holeRadius = 0.8 * player.radius * Math.sqrt(1 - player.buildTimer)
+      context.beginPath()
+      context.arc(x, y, holeRadius, 0, 2 * Math.PI)
+      context.fill()
+    })
+  }
+  if (state.predators) {
+    context.globalAlpha = 1
+    state.predators.forEach(predator => {
+      context.fillStyle = 'red'
+      const x = predator.position.x - camera.position.x
+      const y = predator.position.y - camera.position.y
+      context.beginPath()
+      context.arc(x, y, predator.radius, 0, 2 * Math.PI)
+      context.fill()
+    })
+  }
+}
+
+function draw () {
+  setupCanvas()
+  drawState()
+  window.requestAnimationFrame(draw)
+}
+
+draw()
---
+++ server.js
@@ -0,0 +1,276 @@
+import express from 'express'
+import http from 'http'
+import https from 'https'
+import fs from 'fs-extra'
+import path from 'path'
+import { Server } from 'socket.io'
+import { fileURLToPath } from 'url'
+import { getLength, getDist, getDirection, dot, project, norm, mult, add, sub } from './vector.js'
+import { collide } from './collision.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const config = fs.readJSONSync('config.json')
+console.log(config)
+
+const app = express()
+const staticPath = path.join(__dirname, 'public')
+const staticMiddleware = express.static(staticPath)
+app.use(staticMiddleware)
+const clientHtmlPath = path.join(__dirname, 'public', 'client.html')
+app.get('/', function (req, res) { res.sendFile(clientHtmlPath) })
+const socketIoPath = path.join(__dirname, 'node_modules', 'socket.io', 'client-dist')
+app.get('/socketIo/:fileName', function (req, res) {
+  const filePath = path.join(socketIoPath, req.params.fileName)
+  res.sendFile(filePath)
+})
+
+function makeServer () {
+  if (config.secure) {
+    const key = fs.readFileSync('./srs-key.pem')
+    const cert = fs.readFileSync('./srs-cert.pem')
+    const credentials = { key, cert }
+    return new https.Server(credentials, app)
+  } else {
+    return new http.Server(app)
+  }
+}
+
+const server = makeServer()
+const io = new Server(server)
+io.path(staticPath)
+server.listen(config.port, () => {
+  console.log(`Listening on :${config.port}`)
+  setInterval(tick, dt * 1000)
+})
+
+function range (n) { return [...Array(n).keys()] }
+
+const dt = 0.01
+const drag = 1
+const actorMovePower = 70
+const mapSize = 150
+const actorSize = 1
+const nodeSize = 9
+const nodeSpread = 6
+
+const compass = [
+  { x: 1, y: 0 },
+  { x: -1, y: 0 },
+  { x: 0, y: 1 },
+  { x: 0, y: -1 },
+  { x: Math.sqrt(0.5), y: Math.sqrt(0.5) },
+  { x: -Math.sqrt(0.5), y: Math.sqrt(0.5) },
+  { x: Math.sqrt(0.5), y: -Math.sqrt(0.5) },
+  { x: -Math.sqrt(0.5), y: -Math.sqrt(0.5) }
+]
+
+const state = {
+  time: 0,
+  players: [],
+  nodes: [],
+  walls: [],
+  predators: []
+}
+const players = new Map()
+const predators = new Map()
+const sockets = new Map()
+const units = new Map()
+
+setupWalls()
+setupNodes()
+
+function tick () {
+  state.time += dt
+  movePlayers()
+  movePredators()
+  collide(state)
+  updateClients()
+  pursue()
+}
+
+function pursue () {
+  state.predators.forEach(predator => {
+    const prey = predator.prey
+    const preyDir = getDirection(predator.position, prey.position)
+    const projection = project(prey.velocity, preyDir)
+    const rejection = sub(prey.velocity, projection)
+    const flee = 1 * (dot(norm(projection), preyDir) > 0)
+    const fleeVelocity = add(rejection, mult(projection, flee))
+    const distance = getDist(predator.position, prey.position)
+    const advance = 5 + 0.3 * distance
+    const targetVelocity = add(fleeVelocity, mult(preyDir, advance))
+    const targetForce = sub(targetVelocity, predator.velocity)
+    const best = { align: 0 }
+    compass.forEach(compassDir => {
+      const align = dot(compassDir, targetForce)
+      if (align > best.align) {
+        best.align = align
+        predator.force = norm(compassDir)
+      }
+    })
+  })
+}
+
+function movePlayers () {
+  state.players.forEach(player => {
+    if (player.controls) {
+      const dx = player.controls.right - player.controls.left
+      const dy = player.controls.down - player.controls.up
+      player.force = norm({ x: dx, y: dy })
+      moveActor(player)
+    }
+  })
+}
+
+function movePredators () {
+  state.predators.forEach(predator => {
+    if (predator.freezeTimer <= 0) {
+      moveActor(predator)
+    } else if (predator.freezeTimer > 0) {
+      predator.freezeTimer -= dt
+      predator.freezeTimer = Math.max(0, predator.freezeTimer)
+    }
+  })
+}
+
+function moveActor (actor) {
+  actor.velocity.x += actor.force.x * dt * actorMovePower
+  actor.velocity.y += actor.force.y * dt * actorMovePower
+  actor.position.x += actor.velocity.x * dt
+  actor.position.y += actor.velocity.y * dt
+  actor.velocity = mult(actor.velocity, 1 - dt * drag)
+}
+
+function setupWalls () {
+  const wallThickness = 10
+  const wallPadding = -20
+  const wallLength = mapSize + wallPadding
+  const topWall = {
+    position: { x: 0, y: -0.5 * wallLength },
+    width: wallLength + wallThickness,
+    height: wallThickness,
+    id: state.walls.length,
+    role: 'wall'
+  }
+  state.walls.push(topWall)
+  const bottomWall = {
+    position: { x: 0, y: 0.5 * wallLength },
+    width: wallLength + wallThickness,
+    height: wallThickness,
+    id: state.walls.length,
+    role: 'wall'
+  }
+  state.walls.push(bottomWall)
+  const leftWall = {
+    position: { x: -0.5 * wallLength, y: 0 },
+    width: wallThickness,
+    height: wallLength + wallThickness,
+    id: state.walls.length,
+    role: 'wall'
+  }
+  state.walls.push(leftWall)
+  const rightWall = {
+    position: { x: 0.5 * wallLength, y: 0 },
+    width: wallThickness,
+    height: wallLength + 0.5 * wallThickness,
+    id: state.walls.length,
+    role: 'wall'
+  }
+  state.walls.push(rightWall)
+}
+
+function setupNodes () {
+  range(100000).forEach(i => {
+    const position = {
+      x: (Math.random() - 0.5) * (mapSize - 4 * nodeSize),
+      y: (Math.random() - 0.5) * (mapSize - 4 * nodeSize)
+    }
+    const nodeDistances = state.nodes.map(node => getDist(position, node.position))
+    const minNodeDist = Math.min(...nodeDistances, Infinity)
+    const edgeDistances = nodeDistances.map(dist => Math.abs(dist - 2 * nodeSize))
+    const minEdgeDist = Math.min(...edgeDistances)
+    const centerDist = getLength(position)
+    if (minNodeDist > 1.3 * nodeSpread && minEdgeDist > 0.2 * nodeSpread && centerDist > nodeSpread) {
+      const node = {
+        position,
+        id: state.nodes.length,
+        size: nodeSize,
+        radius: nodeSize,
+        range: nodeSize,
+        fill: 1,
+        income: 0,
+        team: 0,
+        role: 'node',
+        neighborIds: []
+      }
+      state.nodes.push(node)
+    }
+  })
+  range(state.nodes.length).forEach(i => {
+    range(state.nodes.length).forEach(j => {
+      if (i < j) {
+        const a = state.nodes[i]
+        const b = state.nodes[j]
+        const dist = getDist(a.position, b.position)
+        if (dist < 2 * nodeSize) {
+          a.neighborIds.push(b.id)
+          b.neighborIds.push(a.id)
+        }
+      }
+    })
+  })
+}
+
+async function updateClients () {
+  state.players = Array.from(players.values())
+  state.predators = Array.from(predators.values())
+  state.units = Array.from(units.values())
+  players.forEach(player => {
+    const socket = sockets.get(player.id)
+    const msg = { state, position: player.position }
+    socket.emit('updateClient', msg)
+  })
+}
+
+io.on('connection', socket => {
+  console.log('connection:', socket.id)
+  socket.emit('socketId', socket.id)
+  state.players = Array.from(players.values())
+  const teamCount1 = state.players.filter(player => player.team === 1).length
+  const teamCount2 = state.players.filter(player => player.team === 2).length
+  const smallTeam = teamCount1 > teamCount2 ? 2 : 1
+  const player = {
+    id: socket.id,
+    team: smallTeam,
+    buildTimer: 0,
+    position: { x: 0, y: 0 },
+    velocity: { x: 0, y: 0 },
+    force: { x: 0, y: 0 },
+    radius: actorSize,
+    role: 'player'
+  }
+  players.set(socket.id, player)
+  sockets.set(socket.id, socket)
+  const predator = {
+    id: socket.id,
+    team: 3,
+    position: { x: 0, y: 50 },
+    velocity: { x: 0, y: 0 },
+    force: { x: 0, y: 0 },
+    radius: actorSize,
+    prey: player,
+    freezeTimer: 0,
+    role: 'predator'
+  }
+  predators.set(socket.id, predator)
+  socket.on('updateServer', message => {
+    player.controls = message.controls
+  })
+  socket.on('disconnect', () => {
+    console.log('disconnect:', socket.id)
+    sockets.delete(socket.id)
+    players.delete(socket.id)
+    predators.delete(socket.id)
+  })
+})
